2a489895966b3152586ebceb94eb26b310609804
Kedar <nagella.kedarnath@mindtree.com>
2020-04-02 12:48:01 +0530

    added new end point with agent

--- routes/order.js
+++ routes/order.js
@@ -3,12 +3,18 @@ var https = require("https");
 var express = require("express");
 var router = express.Router();
 var axios = require("axios-https-proxy-fix");
+const Agent = require("agentkeepalive");
 
 var utilities = require("../utilities");
 
-//http.globalAgent.maxSockets = Infinity;
-//http.globalAgent.agent = false;
 const keepAliveAgent = new http.Agent({ keepAlive: true, keepAliveMsecs: 100000, maxSockets: 100 });
+
+const keepaliveAgent = new Agent({
+  maxSockets: 100,
+  maxFreeSockets: 10,
+  timeout: 60000, // active socket keepalive for 60 seconds
+  freeSocketTimeout: 30000 // free socket keepalive for 30 seconds
+});
 
 //this proxy setting is for working from home
 var proxy = {
@@ -18,7 +24,7 @@ var proxy = {
 
 //var axiosoptions = { proxy, agent: keepAliveAgent };
 
-var axiosoptions = { proxy };
+var axiosoptions = { proxy, agent: false };
 
 if (process.env.UseProxy === "false" || process.env.UseProxy === undefined) {
   console.log("setting proxy to false");
@@ -26,7 +32,7 @@ if (process.env.UseProxy === "false" || process.env.UseProxy === undefined) {
 }
 
 var localtestApi = "http://localhost:3501/ping";
-var azuretestApi = "https://pingapi1234.azurewebsites.net/ping";
+var azuretestApi = "http://pingapi1234.azurewebsites.net/ping";
 
 var getrul = azuretestApi;
 
@@ -34,6 +40,37 @@ var getrul = azuretestApi;
 router.get("/", async function(req, res, next) {
   await axios.get(getrul, axiosoptions);
   res.status(200).json("from order get method");
+});
+
+router.get("/withAgent", async function(req, res, next) {
+  const options = {
+    host: "pingapi1234.azurewebsites.net",
+    port: 80,
+    path: "/ping",
+    method: "GET",
+    agent: keepAliveAgent
+  };
+
+  await new Promise((resolve, reject) => {
+    const request = http.request(options, response => {
+      //console.log("STATUS: " + res.statusCode);
+      //console.log("HEADERS: " + JSON.stringify(res.headers));
+      response.setEncoding("utf8");
+      response.on("data", function(chunk) {
+        //  console.log("BODY: " + chunk);
+        resolve(chunk);
+      });
+    });
+    request.on("error", e => {
+      console.log("problem with request: " + e.message);
+    });
+    request.end();
+  });
+
+  //console.log("completing request");
+
+  //await axios.get(getrul, axiosoptions);
+  res.status(200).json("from order get method with agent");
 });
 
 module.exports = router;
